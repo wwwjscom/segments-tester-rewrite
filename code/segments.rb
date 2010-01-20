a6cf3e376199e9d8f8f72c6e3b4024300cf88861
Jason Soo <wwwjscom@gmail.com>
2010-01-19 20:06:58 -0600

    segments find

---
+++ code/segments.rb
@@ -0,0 +1,15 @@
+class Segments
+	
+	def find(query)
+		@candidates = {}
+		`php lib/segments/searchresults.php "#{query}"`
+		file = File.open('tmp/our_results.txt')
+		while line = file.gets do
+			line.chomp!
+			votes = line.split(', ')[0].to_i
+			condidate = line.split(', ')[1]
+			@candidates[condidate] = votes
+		end
+		@candidates
+	end
+end
---
+++ tests/segments_spec.rb
@@ -0,0 +1,17 @@
+require "code/segments"
+
+describe Segments do
+	
+	before(:all) do
+		@seg = Segments.new
+	end
+	
+	it 'should find' do
+		results = @seg.find('query0')
+		results.class.should == Hash
+		results['query0'].should == 6
+		results['query1'].should == 3
+		results['query2'].should == 3
+	end
+	
+end
